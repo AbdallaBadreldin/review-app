a2a3b40b6783493d0b67897b573a60d4afca7c6c
Abdalla badr <Bossmail28@gmail.com>
2022-07-02 21:28:22 +0200

    prepared user and his data, also item and empty recyclerview

--- app/src/main/java/com/Fstech/review/ui/home/HomeFragment.kt
+++ app/src/main/java/com/Fstech/review/ui/home/HomeFragment.kt
@@ -8,6 +8,13 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.fstech.review.databinding.FragmentHomeBinding
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.core.DatabaseConfig
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment : Fragment() {
 
@@ -16,12 +23,17 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var database: DatabaseReference
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        database = Firebase.database.reference
+
+
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
@@ -33,6 +45,41 @@ class HomeFragment : Fragment() {
             textView.text = it
         }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val database = Firebase.database
+        val myRef = database.getReference("users")
+
+//        val myUserId = uid
+//        val myTopPostsQuery = databaseReference.child("user-posts").child(myUserId)
+//            .orderByChild("starCount")
+
+        val myMostViewedPostsQuery = myRef.orderByChild("metrics/views")
+        myMostViewedPostsQuery.addChildEventListener(object : ChildEventListener {
+            // TODO: implement the ChildEventListener methods as documented above
+            // ...
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                //TODO("Not yet implemented")
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                //TODO("Not yet implemented")
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                //TODO("Not yet implemented")
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                //TODO("Not yet implemented")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                //TODO("Not yet implemented")
+            }
+        })
     }
 
     override fun onDestroyView() {
